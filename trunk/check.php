faf7538332a89e96a72cc8ea8765050c3b742619
topsvr <topsvr@d94f786a-dd63-11de-b357-fb893c5a43fa>
2010-03-19 13:14:01 +0000

    fixbug sql语句错误 utf8字符集错误 xingTemplate字符问题

--- trunk/check.php
+++ trunk/check.php
@@ -1,9 +1,8 @@
 <?
 include_once 'tis.php';
-if (!$_SESSION[logname]) 
-{
-$tis= '您没有登录，还不能进行此操作';
-tis($tis);
-exit;
+if (!$_SESSION[logname]){
+	$tis= '您没有登录，还不能进行此操作';
+	tis($tis);
+	exit;
 }
 ?>
