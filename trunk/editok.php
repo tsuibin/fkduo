32dcdf292934594b94d5ef11adef4249c559aff6
topsvr <topsvr@d94f786a-dd63-11de-b357-fb893c5a43fa>
2010-03-20 03:35:29 +0000

    修正用户控制面板提交，提示错误 目前没有做提交失败错误判断处理

--- trunk/editok.php
+++ trunk/editok.php
@@ -1,20 +1,20 @@
-<?
+<?php
 $yanc=1;
 include 'conn.php';
 include 'check.php' ;
 
 if (empty($_POST['message']) or empty($_GET['cid'])) {
-$tis= "参数错误，请从正常页面进行操作"; 
-tis($tis);
-exit;
+	$tis= "参数错误，请从正常页面进行操作"; 
+	tis($tis);
+	exit;
 }
 
 $_POST['title']=trim($_POST['title']);
 
 if (empty($_POST['title'])){
-$tis="对不起，标题不能为空！";
-tis($tis);
-exit;
+	$tis="对不起，标题不能为空！";
+	tis($tis);
+	exit;
 }
 
 
@@ -22,9 +22,9 @@ $query11=mysql_query("select * from `{$fkduo}user` where `logname`='$_SESSION[lo
 $row11=mysql_fetch_array($query11) ;
 
 if ($row11[lock]==1){
-$tis="对不起，你的账号处于冻结期，无法发贴！";
-tis($tis);
-exit;
+	$tis="对不起，你的账号处于冻结期，无法发贴！";
+	tis($tis);
+	exit;
 }
 
 
@@ -37,7 +37,7 @@ $title=addslashes($title);
 
 $content=$_POST['message'];
 $content=htmlentities($content, ENT_QUOTES,utf8);
-$content=str_replace("\r\n","<br />",$content); 
+$content=str_replace("\r\n","<br />",$content);
 $content=addslashes($content);
 
 
@@ -45,7 +45,7 @@ include 'include/replace.php';//进行审核过滤词语处理
 
 
 if (!($_POST['closecode']==1)){
-include 'include/ubb.php';
+	include 'include/ubb.php';
 }
 
 
@@ -57,19 +57,17 @@ $lc=(int)($_GET['lc']);
 
 if ($_GET['action']==lz){
 
-$sql="update `{$fkduo}zhuti` set `title`='$title',`content`='$content',`edit`='$edittime',`through`='$through',`edits`=`edits`+1 where (`cid`='$cid')";
+	$sql="update `{$fkduo}zhuti` set `title`='$title',`content`='$content',`edit`='$edittime',`through`='$through',`edits`=`edits`+1 where (`cid`='$cid')";
 
-$query=mysql_query($sql);//
+	$query=mysql_query($sql);
+	
+	$eee="postalert.php?action=lz&cid=".$cid."&bk=".$bk;
 
+	header ("location: $eee");
 
+	$tis="主题成功";
 
-$eee="postalert.php?action=lz&cid=".$cid."&bk=".$bk;
-
-header ("location: $eee"); 
-
-$tis="主题成功";
-
-tis($tis);
+	tis($tis);
 
 }
 
@@ -77,23 +75,23 @@ tis($tis);
 
 
 
-elseif($_GET['action']==huifu)
+elseif($_GET['action']=='huifu')
 
 {
 
-$sql="update `{$fkduo}card` set `content`='$content',`edit`='$edittime',`through`='$through',`edits`=`edits`+1 where (`cid`='$cid' and `lc`='$lc')";
+	$sql="update `{$fkduo}card` set `content`='$content',`edit`='$edittime',`through`='$through',`edits`=`edits`+1 where (`cid`='$cid' and `lc`='$lc')";
 
-$query=mysql_query($sql);//
+	$query=mysql_query($sql);//
 
 
 
-$eee="postalert.php?action=huifu&cid=".$cid."&bk=".$_GET['bk']."&now=".$_GET['now']."&pid=".$lc;
+	$eee="postalert.php?action=huifu&cid=".$cid."&bk=".$_GET['bk']."&now=".$_GET['now']."&pid=".$lc;
 
-header ("location: $eee"); 
+	header ("location: $eee");
 
-$tis="回复成功";
+	$tis="回复成功";
 
-tis($tis);
+	tis($tis);
 
 }
 
