d0ce1ce6a454865b259bb1e9efec9494f80f5d85
topsvr <topsvr@d94f786a-dd63-11de-b357-fb893c5a43fa>
2010-03-21 12:29:43 +0000

    添加png头像格式支持

--- trunk/myok.php
+++ trunk/myok.php
@@ -1,4 +1,4 @@
-<?
+<?php
 include 'conn.php';
 include 'check.php';
 
@@ -7,12 +7,12 @@ if ($_GET['action']==send){//SMS发送处理
 	$from=$_SESSION[logname];
 
 	$title=$_POST['title'];
-	$title=htmlentities($title, ENT_QUOTES,utf8);
+	$title=htmlentities($title, ENT_QUOTES,'UTF-8');
 	$title=str_replace("\r\n","",$title);
 	$title=addslashes($title);
 
 	$content=$_POST['content'];
-	$content=htmlentities($content, ENT_QUOTES,utf8);
+	$content=htmlentities($content, ENT_QUOTES,'UTF-8');
 	$content=str_replace("\r\n","<br />",$content);
 	$content=addslashes($content);
 
@@ -181,15 +181,19 @@ if ($_GET['action']==send){//SMS发送处理
 			switch ($type) {
 				case 'image/pjpeg' : $ok=1;
 				$name=$_SESSION[logname].".jpg";
-				$face=jpg;
+				$face='jpg';
 				break;
 				case 'image/jpeg' : $ok=1;
 				$name=$_SESSION[logname].".jpg";
-				$face=jpg;
+				$face='jpg';
 				break;
 				case 'image/gif' : $ok=1;
 				$name=$_SESSION[logname].".gif";
-				$face=gif;
+				$face='gif';
+				break;
+				case 'image/png' : $ok=1;
+				$name=$_SESSION[logname].".png";
+				$face='png';
 				break;
 				default:
 					echo "不允许的文件内型";
